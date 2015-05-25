14f2d78d1db71db1614a31c8c244511f02a465b7
candale <candale.andrei@gmail.com>
2015-05-25 17:13:47 +0300

    Tried to make Arduino firmware to work. No success

--- impl/esp/arduino/arduino_raw/firmware/firmware.ino
+++ impl/esp/arduino/arduino_raw/firmware/firmware.ino
@@ -22,12 +22,12 @@ void setup_mdns()
 {
   if (!mdns.begin("thermostat", WiFi.localIP())) {
     Serial.println("Error setting up MDNS responder!");
-    while(1) { 
+    while(1) {
       delay(1000);
     }
   }
   Serial.println("mDNS responder started");
- 
+
   // Start TCP (HTTP) server
   web_server.begin();
   Serial.println("TCP server started");
@@ -46,10 +46,10 @@ void serve_network_client()
   while(client.connected() && !client.available()){
     delay(1);
   }
-  
+
   // Read the first line of HTTP request
   String req = client.readStringUntil('\r');
-  
+
   // First line of HTTP request looks like "GET /path HTTP/1.1"
   // Retrieve the "/path" part by finding the spaces
   int addr_start = req.indexOf(' ');
@@ -63,7 +63,7 @@ void serve_network_client()
   Serial.print("Request: ");
   Serial.println(req);
   client.flush();
-  
+
   String s;
   if (req == "/")
   {
@@ -81,7 +81,7 @@ void serve_network_client()
   }
   Serial.println(ESP.getFreeHeap());
   client.print(s);
-  
+
   Serial.println("Done with client");
 }
 
@@ -89,48 +89,50 @@ void connect_to_wifi()
 {
   // Connect to WiFi network
   WiFi.begin(wifi_network_ssid, wifi_network_password);
-  Serial.println("");  
-  
+  Serial.println("");
+
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
     Serial.print(WiFi.status());
   }
-  Serial.println("");/
+  Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
+void handleRoot() {
+  wifi_server.send(200, "text/html",
+    "<html>"
+    "<h4>Connected to wifi: %s</h4>");
+}
+
 void setup_softap()
 {
   Serial.print("Configuring access point...");\
   WiFi.softAP(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) { 
-    delay(500);
-    Serial.print(".");
-  }
-  
+
   Serial.println("done");
   IPAddress myIP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(myIP);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    Serial.print(WiFi.status());
+  }
 
-  wifi_server.on("/", handleRoot);
+  wifi_server.on("/connect", handleRoot);
   wifi_server.begin();
 
   Serial.println("HTTP server started");
 }
 
-void handleRoot() {
-	wifi_server.send(200, "text/html",
-    "<html>"
-    "<h4>Connected to wifi: %s</h4>");
-}
-
 void setup() {
+  WiFi.mode(WIFI_AP_STA);
   delay(1000);
   Serial.begin(115200);
   Serial.println();
