948a0c1725ecefbb4a1712176f2189e0a32d6666
candale <candale.andrei@gmail.com>
2015-06-01 21:24:13 +0300

    wqArticle done

--- doc/article/articol/run.sh
+++ doc/article/articol/run.sh
@@ -1,10 +1,7 @@
-fis=scss2015as
+fis=scss2015ac
 latex -interaction=nonstopmode $fis.tex
 bibtex $fis.aux
 latex -interaction=nonstopmode $fis.tex
 latex -interaction=nonstopmode $fis.tex
 dvips -o $fis.ps $fis.dvi
 ps2pdf $fis.ps
-
-
-
